fecf004dd62c8ef94748f805fc432ade4c94391d
IngPipex06 <fellippex06@hotmail.com>
2025-04-08 13:56:54 -0500

    changed some important code, made code easier to read

--- ConstructorCombo.java
+++ ConstructorCombo.java
@@ -4,19 +4,20 @@
 public class ConstructorCombo {
     private Combo comboActual;
     
+    //Method that initializes the combo, receives nombre var.
     public void iniciarCombo(String nombre) {
-        comboActual = new Combo(nombre);
+      comboActual = new Combo(nombre);
     }
     
     // Se recibe un objeto Producto, ya que según el diagrama sólo se agregan productos
     public void agregarProducto(Producto producto) {
-        if (comboActual != null) {
-            comboActual.agregarItem(producto);
-        }
+      if (comboActual != null) {
+        comboActual.agregarItem(producto);
+      }
     }
     
     public Combo obtenerCombo() {
-        return comboActual;
+      return comboActual;
     }
 }
 
